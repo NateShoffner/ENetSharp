5b38fe4af4e61af286b65867a1a3963a2379bf9c
Nate Shoffner <nate.shoffner@gmail.com>
2016-03-04 14:17:50 -0500

    AssemblyInformationalVersion

--- ENetCS/Properties/AssemblyInfo.cs
+++ ENetCS/Properties/AssemblyInfo.cs
@@ -8,4 +8,5 @@ using System.Reflection;
 [assembly: AssemblyProduct("ENet for C#")]
 [assembly: AssemblyCopyright("Copyright © 2011 James F. Bellinger <jfb@zer7.com>")]
 [assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyInformationalVersion("1.3.1.0")]
